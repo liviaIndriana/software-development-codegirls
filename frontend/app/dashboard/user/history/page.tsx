e0ea1fd01fd8d80f34e9925535b7d8b94bce9204
RismaShatiya <rismashatiya04@gmail.com>
2026-06-02 21:18:17 +0700

    add: menambahkan halaman history pada dashboard user

---
+++ frontend/app/dashboard/user/history/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useAuth } from "@/features/auth/login/hooks/use-auth";
+import UserHistory from "@/features/user-history/components/UserHistory";
+
+
+
+
+export default function UserHistoryPage() {
+  const { isChecking } = useAuth();
+  if (isChecking) return null; 
+  return <UserHistory/> 
+}
